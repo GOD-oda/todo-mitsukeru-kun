314a57e91dbf2c5ea9c2507cbed6dbbe3dad951c
gododa <last.pass.oda@gmail.com>
2024-01-06 10:09:46 +0900

    Update build paths and commands for multi-OS compatibility
    Paths in 'action.yaml', 'build.sh', 'compose.yaml', and 'Dockerfile' have been updated to facilitate multi-OS builds of the 'todo-mitsukeru-kun' application. This includes adjustments to the Go build command to generate Linux and Darwin (MacOS) specific binaries, enhancing the build process to function smoothly across these platforms.

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-env GOOS=linux GOARCH=amd64 go build
+NAME="todo-mitsukeru-kun"
+
+env GOOS=linux GOARCH=amd64 go build -o bin/dist/linux/${NAME}
+env GOOS=darwin GOARCH=amd64 go build -o bin/dist/darwin/${NAME}
